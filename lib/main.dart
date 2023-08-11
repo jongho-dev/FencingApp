5db9ae8ab94674ed78f3e3d8ba349f640ad77706
JongHoPark <qkrwhdgh126@gmail.com>
2023-08-11 22:03:59 +0900

    FEAT: Tabbar 구현

--- lib/main.dart
+++ lib/main.dart
@@ -10,27 +10,54 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'FencingApp',
       debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        appBar: AppBar(
-          shape: Border(
-              bottom: BorderSide(
-            color: Colors.grey,
-            width: 0.7,
-          )),
-          iconTheme: IconThemeData(
-            color: Colors.black,
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return DefaultTabController(
+      initialIndex: 1,
+      length: 3,
+      child: Scaffold(
+          appBar: AppBar(
+            backgroundColor: Colors.white,
+            elevation: 0.0,
+            shape: Border(bottom: BorderSide(color: Colors.grey, width: 1.0)),
+            iconTheme: IconThemeData(color: Colors.black),
+            actions: [
+              IconButton(
+                icon: Icon(Icons.search),
+                onPressed: () {},
+              )
+            ],
+            bottom: TabBar(
+              tabs: [
+                Tab(text: 'MENU1'),
+                Tab(text: 'MENU2'),
+                Tab(text: 'MENU3'),
+              ],
+              labelColor: Colors.black,
+              indicatorColor: Colors.redAccent,
+            ),
           ),
-          elevation: 0.0,
-          backgroundColor: Colors.white,
-          actions: [
-            IconButton(
-              icon: Icon(Icons.search),
-              onPressed: () {},
-            )
-          ],
-        ),
-        drawer: Drawer(),
-      ),
+          drawer: Drawer(),
+          body: TabBarView(
+            children: [
+              Center(child: Text('Page1')),
+              Center(child: Text('Page2')),
+              Center(child: Text('Page3')),
+            ],
+          )),
     );
   }
 }
