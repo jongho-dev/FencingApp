ac51e8c2e8fc023ba85c801353dde1dc08b06517
JongHoPark <qkrwhdgh126@gmail.com>
2023-08-13 22:56:51 +0900

    FIX: BottomNavigateBar 수정

--- lib/main.dart
+++ lib/main.dart
@@ -34,27 +34,13 @@ class _HomePageState extends State<HomePage> {
           elevation: 0.0,
           shape: Border(bottom: BorderSide(color: Colors.grey, width: 1.0)),
           iconTheme: IconThemeData(color: Colors.black),
-          // actions: [
-          //   IconButton(
-          //     icon: Icon(Icons.search),
-          //     onPressed: () {},
-          //   ),
-          //   IconButton(
-          //     icon: Icon(Icons.person),
-          //     onPressed: () {},
-          //   )
-          // ],
-          // bottom: TabBar(
-          //   tabs: [
-          //     Tab(text: '홈'),
-          //     Tab(text: 'MENU2'),
-          //     Tab(text: 'MENU3'),
-          //   ],
-          //   labelColor: Colors.black,
-          //   indicatorColor: Colors.redAccent,
-          // ),
+          actions: [
+            IconButton(
+              icon: Icon(Icons.search),
+              onPressed: () {},
+            ),
+          ],
         ),
-        endDrawer: Drawer(),
         body: TabBarView(
           children: [
             Center(child: Text('Home Page')),
@@ -69,7 +55,10 @@ class _HomePageState extends State<HomePage> {
           child: TabBar(
             tabs: [
               Tab(
-                icon: Icon(Icons.home),
+                icon: Icon(
+                  Icons.home,
+                  size: 25,
+                ),
                 text: '홈',
               ),
               Tab(
@@ -77,11 +66,17 @@ class _HomePageState extends State<HomePage> {
                 text: '글',
               ),
               Tab(
-                icon: Icon(Icons.chat_bubble),
+                icon: Icon(
+                  Icons.chat_bubble,
+                  size: 21,
+                ),
                 text: '채팅',
               ),
               Tab(
-                icon: Icon(Icons.person),
+                icon: Icon(
+                  Icons.person,
+                  size: 27,
+                ),
                 text: '프로필',
               ),
             ],
