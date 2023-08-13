f772a418a62ae274c335a3f189bfc3b7c0fe93b4
JongHoPark <qkrwhdgh126@gmail.com>
2023-08-13 22:23:26 +0900

    FEAT: BottomNavigateBar 구현

--- lib/main.dart
+++ lib/main.dart
@@ -27,37 +27,71 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     return DefaultTabController(
       initialIndex: 1,
-      length: 3,
+      length: 4,
       child: Scaffold(
-          appBar: AppBar(
-            backgroundColor: Colors.white,
-            elevation: 0.0,
-            shape: Border(bottom: BorderSide(color: Colors.grey, width: 1.0)),
-            iconTheme: IconThemeData(color: Colors.black),
-            actions: [
-              IconButton(
-                icon: Icon(Icons.search),
-                onPressed: () {},
-              )
+        appBar: AppBar(
+          backgroundColor: Colors.white,
+          elevation: 0.0,
+          shape: Border(bottom: BorderSide(color: Colors.grey, width: 1.0)),
+          iconTheme: IconThemeData(color: Colors.black),
+          // actions: [
+          //   IconButton(
+          //     icon: Icon(Icons.search),
+          //     onPressed: () {},
+          //   ),
+          //   IconButton(
+          //     icon: Icon(Icons.person),
+          //     onPressed: () {},
+          //   )
+          // ],
+          // bottom: TabBar(
+          //   tabs: [
+          //     Tab(text: '홈'),
+          //     Tab(text: 'MENU2'),
+          //     Tab(text: 'MENU3'),
+          //   ],
+          //   labelColor: Colors.black,
+          //   indicatorColor: Colors.redAccent,
+          // ),
+        ),
+        endDrawer: Drawer(),
+        body: TabBarView(
+          children: [
+            Center(child: Text('Home Page')),
+            Center(child: Text('Edit Page')),
+            Center(child: Text('Chatting Page')),
+            Center(child: Text('Profile Page'))
+          ],
+        ),
+        bottomNavigationBar: Container(
+          color: Colors.white,
+          height: 80,
+          child: TabBar(
+            tabs: [
+              Tab(
+                icon: Icon(Icons.home),
+                text: '홈',
+              ),
+              Tab(
+                icon: Icon(Icons.edit),
+                text: '글',
+              ),
+              Tab(
+                icon: Icon(Icons.chat_bubble),
+                text: '채팅',
+              ),
+              Tab(
+                icon: Icon(Icons.person),
+                text: '프로필',
+              ),
             ],
-            bottom: TabBar(
-              tabs: [
-                Tab(text: 'MENU1'),
-                Tab(text: 'MENU2'),
-                Tab(text: 'MENU3'),
-              ],
-              labelColor: Colors.black,
-              indicatorColor: Colors.redAccent,
-            ),
+            labelColor: Colors.redAccent,
+            labelStyle: TextStyle(fontSize: 14),
+            indicatorColor: Colors.redAccent,
+            unselectedLabelColor: Colors.grey[600],
           ),
-          drawer: Drawer(),
-          body: TabBarView(
-            children: [
-              Center(child: Text('Page1')),
-              Center(child: Text('Page2')),
-              Center(child: Text('Page3')),
-            ],
-          )),
+        ),
+      ),
     );
   }
 }
