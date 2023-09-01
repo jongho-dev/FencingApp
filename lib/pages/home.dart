d4fd6a48130daf5bac67af7c4047b5e94e31d452
JongHoPark <qkrwhdgh126@gmail.com>
2023-09-01 23:42:58 +0900

    FIX: home 페이지 수정

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+// 파이어베이스 패키지
 import 'package:cloud_firestore/cloud_firestore.dart';
 
 class HomePage extends StatefulWidget {
@@ -12,67 +13,50 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          backgroundColor: Colors.white,
-          elevation: 0.0,
-          // shape: Border(bottom: BorderSide(color: Colors.grey, width: 1.0)),
-          iconTheme: IconThemeData(color: Colors.black),
-          actions: [
-            IconButton(
-              icon: Icon(Icons.search),
-              onPressed: () {},
-            ),
-          ],
-        ),
-        body: StreamBuilder(
-          stream: product.snapshots(),
-          builder: (BuildContext context,
-              AsyncSnapshot<QuerySnapshot> streamSnapshot) {
-            print(streamSnapshot.hasError);
-            if (streamSnapshot.hasData) {
-              return ListView.builder(
-                itemCount: streamSnapshot.data!.docs.length,
-                itemBuilder: (context, index) {
-                  final DocumentSnapshot documentSnapshot =
-                      streamSnapshot.data!.docs[index];
-                  return Card(
-                    child: ListTile(
-                      title: Text(documentSnapshot['content']),
-                      subtitle: Text(documentSnapshot['writer']),
-                    ),
-                  );
-                },
-              );
-            }
-            return Center(
-              child: Text('데이터 없음요'),
+      appBar: AppBar(
+        automaticallyImplyLeading: false,
+        backgroundColor: Colors.white,
+        elevation: 0.0,
+        shape: Border(bottom: BorderSide(color: Colors.grey, width: 1.0)),
+        iconTheme: IconThemeData(color: Colors.black),
+        actions: [
+          IconButton(
+            icon: Icon(Icons.search),
+            onPressed: () {},
+          ),
+        ],
+      ),
+      body: StreamBuilder(
+        stream: product.snapshots(),
+        builder: (BuildContext context,
+            AsyncSnapshot<QuerySnapshot> streamSnapshot) {
+          print(streamSnapshot.hasError);
+          if (streamSnapshot.hasData) {
+            return ListView.builder(
+              itemCount: streamSnapshot.data!.docs.length,
+              itemBuilder: (context, index) {
+                final DocumentSnapshot documentSnapshot =
+                    streamSnapshot.data!.docs[index];
+                return Card(
+                  child: ListTile(
+                    title: Text(documentSnapshot['content']),
+                    subtitle: Text(documentSnapshot['writer']),
+                  ),
+                );
+              },
             );
-          },
-        )
-        // body: ListView.builder(
-        //   itemCount: postlist.length,
-        //   itemBuilder: (context, index) {
-        //     var name = postlist[index]["title"];
-        //     return Container(
-        //       height: 150,
-        //       decoration: BoxDecoration(
-        //           color: Colors.white,
-        //           border:
-        //               Border(bottom: BorderSide(color: Colors.grey, width: 1.0))),
-        //       alignment: Alignment.topLeft,
-        //       child: Padding(
-        //         padding: const EdgeInsets.fromLTRB(15, 20, 15, 10),
-        //         child: Text(
-        //           '$name',
-        //           style: TextStyle(
-        //             fontWeight: FontWeight.bold,
-        //             fontSize: 17,
-        //           ),
-        //         ),
-        //       ),
-        //     );
-        //   },
-        // ),
-        );
+          }
+          return Center(
+            child: Text('데이터 없음요'),
+          );
+        },
+      ),
+      floatingActionButton: FloatingActionButton.extended(
+        icon: Icon(Icons.add, size: 22.0),
+        label: Text('글쓰기', style: TextStyle(fontSize: 20.0)),
+        backgroundColor: Colors.blue,
+        onPressed: () {},
+      ),
+    );
   }
 }
